b0cf8d87f712da2e8d8fe44230654ce0d5ed30c9
xcasper <514644743@qq.com>
2020-03-23 17:04:29 +0800

    first commit

---
+++ src/main/java/org/casper/Test1.java
@@ -0,0 +1,17 @@
+/*
+ * @Description:
+ * 
+ * @Autor: maxp
+ * 
+ * @Date: 2020-03-23 17:02:29
+ * 
+ * @LastEditTime: 2020-03-23 17:02:40
+ */
+package org.casper;
+
+/**
+ * Test1
+ */
+public class Test1 {
+
+}
